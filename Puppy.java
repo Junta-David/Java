884cd94674f186136d224d0d22e1d26c6b1257ab
ronald-killadesigns <ronald@killadesigns>
2016-03-03 18:05:52 +0300

    second file

---
+++ Puppy.java
@@ -0,0 +1,24 @@
+public class Puppy{
+int puppyAge;
+public puppy(String name){
+// This constructor has one parameter, name.
+System.out.println("Passed Name is :"+ name );
+}
+public void setAge(int age ){
+puppyAge = age;
+}
+public int getAge(){
+System.out.println("Puppy's age is :"+ puppyAge );
+return puppyAge;
+}
+ public static void main(String[]args){
+/* Object creation */
+Puppy myPuppy =newPuppy("tommy");
+/* Call class method to set puppy's age */
+myPuppy.setAge(2);
+/* Call another class method to get puppy's age */
+ myPuppy.getAge();
+/* You can access instance variable as follows as well */
+System.out.println("Variable Value :"+ myPuppy.puppyAge );
+}
+}
